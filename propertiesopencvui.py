00cd4ed04a075260537596d8d49a40ef81d59966
opgbaudouin <opgbaudouin@uclan.ac.uk>
2017-12-14 14:36:10 +0000

    Cleaner PropertyUI implementation

--- propertiesopencvui.py
+++ propertiesopencvui.py
@@ -2,6 +2,16 @@ from properties import *
 import cv2
 
 
+class PropertyUIOpenCV:
+    def __init__(self, name: str, window_name: str):
+        self.name = name
+        self.window_name = window_name
+        self.children = []
+
+    def add(self, ui: "PropertyUIOpenCV"):
+        self.children.append(ui)
+
+
 class PropertiesOpenCVUI(PropertiesListener):
     def __init__(self, properties_obj):
         self.properties = properties_obj
@@ -10,70 +20,69 @@ class PropertiesOpenCVUI(PropertiesListener):
         self.properties_uis = dict()
 
     def on_prop_updated(self, prop: PropertyNode, from_runtime_change: bool = True):
+        """
+        Called when a property value is changed - implements PropertiesListener
+        :param prop:
+        :param from_runtime_change:
+        :return:
+        """
         self.update_ui(prop.path_as_str())
 
-    def nothing(self, x):
-        pass
-
-    def create_bar(self, prop_name: str, window_name: str, minimum, maximum, callback):
-        if callback is None:
-            callback = self.nothing
-        cv2.createTrackbar(prop_name, window_name, minimum, maximum, callback)
-
-    def set_bar_pos(self, prop_name, window_name, value, silent=False):
+    def set_bar_pos(self, prop_name, window_name, value):
         current_value = self.get_bar_pos(prop_name, window_name)
         if current_value != value:
-            # todo - detach event handler that will trigger this. we do not really want to be notified...
-            current_state = self.state
-            if silent:
-                self.state = "SILENT"
-
             cv2.setTrackbarPos(prop_name, window_name, value)
-
-            self.state = current_state
-
 
     def get_bar_pos(self, prop_name, window_name):
         return cv2.getTrackbarPos(prop_name, window_name)
 
-    def prop_call_back(self, prop: Property, ui):
-        # lets NOT read individuals but read the whole prop.
-        #if self.state == "LOADING":
-        #    return
+    def bar_call_back(self, prop: Property, ui: PropertyUIOpenCV):
         if self.state == "SILENT":
             return
         prop.set(self.get_value_from_ui(prop, ui))
 
+    def create_ui_bar(self, ui: PropertyUIOpenCV, prop: Property, min, max, prop_ui=None):
+        callback_ui = ui
+        if prop_ui is not None:
+            callback_ui = prop_ui
+        cv2.createTrackbar(ui.name, ui.window_name, min, max,
+                           lambda x, p=prop, ui=callback_ui: self.bar_call_back(p, ui))
 
-    def create_ui_for_property(self, prop: Property, window_name):
+    def ui_for_rgb(self, prop: Property, window_name: str) -> PropertyUIOpenCV:
+        ui = PropertyUIOpenCV(prop.name, window_name)
+        for i in range(0, len(prop.min)):
+            child_ui = PropertyUIOpenCV(prop.name + '_' + prop.names[i], window_name)
+            ui.add(child_ui)
+            self.create_ui_bar(child_ui, prop, prop.min[i], prop.max[i], ui)
+        return ui
+
+    def ui_for_hsv(self, prop: Property, window_name: str) -> PropertyUIOpenCV:
+        ui = PropertyUIOpenCV(prop.name, window_name)
+        for i in range(0, len(prop.min)):
+            child_ui = PropertyUIOpenCV(prop.name + '_' + prop.names[i], window_name)
+            ui.add(child_ui)
+            max = int(prop.max[i])
+            if i > 0:
+                max = int(max * 255)  # S/V are 0 to 1 so we map to 0 -> 255, can be anythoing though... 100?
+            self.create_ui_bar(child_ui, prop, int(prop.min[i]), max, ui)
+        return ui
+
+    def ui_for_prop(self, prop: Property, window_name: str) -> PropertyUIOpenCV:
+        ui = PropertyUIOpenCV(prop.name, window_name)
+        self.create_ui_bar(ui, prop, prop.min, prop.max)
+        return ui
+
+    def create_ui_for_property(self, prop: Property, window_name) -> PropertyUIOpenCV:
         if prop.type == PropNodeType.rgb:
-            ui = dict(name=prop.name, window_name=window_name, children_ui=[])  # composite UI
-            for i in range(0, len(prop.min)):
-                child_ui = dict(name=prop.name + '_' + prop.names[i], window_name=window_name)
-                ui["children_ui"].append(child_ui)
-                self.create_bar(child_ui["name"], child_ui["window_name"], prop.min[i], prop.max[i],
-                                lambda x, p=prop, ui=ui: self.prop_call_back(p, ui))
+            ui = self.ui_for_rgb(prop, window_name)
         elif prop.type == PropNodeType.hsv:
-            ui = dict(name=prop.name, window_name=window_name, children_ui=[])  # composite UI
-            for i in range(0, len(prop.min)):
-                child_ui = dict(name=prop.name + '_' + prop.names[i], window_name=window_name)
-                ui["children_ui"].append(child_ui)
-                min = int(prop.min[i])
-                max = int(prop.max[i])
-                if i > 0:
-                    max = int(max * 255)  # S/V are 0 to 1 so we map to 0 -> 255, can be anythoing though... 100?
-                self.create_bar(child_ui["name"], child_ui["window_name"], min, max,
-                                lambda x, p=prop, ui=ui: self.prop_call_back(p, ui))
+            ui = self.ui_for_hsv(prop, window_name)
         else:
-            ui = dict(name=prop.name, window_name=window_name)
-            self.create_bar(ui["name"], ui["window_name"], prop.min, prop.max,
-                            lambda x, p=prop, ui=ui: self.prop_call_back(p, ui))
+            ui = self.ui_for_prop(prop, window_name)
         return ui
 
     def show(self, node_path: str):
-        #
-        # print("show "+node_path)
-        # self.state = "LOADING"
+        # todo: prevent double showing?
         node = self.properties.node_at_path(node_path)
         if isinstance(node, Properties):
             window_name = node_path
@@ -85,9 +94,7 @@ class PropertiesOpenCVUI(PropertiesListener):
                 else:
                     print("Cannot show SUB groups in OPEN_CV...")
 
-
         self.update_ui(node_path)
-        # self.state = None
 
     def get_uis_for_property(self, prop: Property):
         path = prop.path_as_str()
@@ -95,41 +102,58 @@ class PropertiesOpenCVUI(PropertiesListener):
             return self.properties_uis[path]
         return None
 
-    def add_ui_for_property(self, prop: Property, ui):
+    def add_ui_for_property(self, prop: Property, ui: PropertyUIOpenCV):
         # keep track of UI elements...
         path = prop.path_as_str()
         if path not in self.properties_uis:
             self.properties_uis[path] = []
         self.properties_uis[path].append(ui)
 
-    def get_value_from_ui(self, prop: Property, ui):
-        if prop.type == PropNodeType.hsv:
-            children_ui = ui["children_ui"]
-            h = float(self.get_bar_pos(children_ui[0]["name"], children_ui[0]["window_name"]))
-            s = float(self.get_bar_pos(children_ui[1]["name"], children_ui[1]["window_name"])) / 255.0
-            v = float(self.get_bar_pos(children_ui[2]["name"], children_ui[2]["window_name"])) / 255.0
-            return h, s, v
-        elif prop.type == PropNodeType.rgb:
-            children_ui = ui["children_ui"]
-            r = int(self.get_bar_pos(children_ui[0]["name"], children_ui[0]["window_name"]))
-            g = int(self.get_bar_pos(children_ui[1]["name"], children_ui[1]["window_name"]))
-            b = int(self.get_bar_pos(children_ui[2]["name"], children_ui[2]["window_name"]))
-            return r, g, b  # todo: list to tuple.
-        else:
-            return self.get_bar_pos(ui["name"], ui["window_name"])
+    def set_bar_pos_for_ui(self, ui: PropertyUIOpenCV, value):
+        self.set_bar_pos(ui.name, ui.window_name, value)
 
-    def update_value_for_ui(self, prop: Property, ui):
+    def get_bar_pos_for_ui(self, ui: PropertyUIOpenCV):
+        return self.get_bar_pos(ui.name, ui.window_name)
+
+    def ui_values_from_ui_children(self, ui: PropertyUIOpenCV, prop: Property) -> List:
+        p = []
+        for i in range(0, len(prop.min)):
+            p.append(self.get_bar_pos_for_ui(ui.children[i]))
+        return p
+
+    def set_ui_values_for_ui_children(self, ui: PropertyUIOpenCV, ui_values):
+        for i in range(0, len(ui.children)):
+            self.set_bar_pos_for_ui(ui.children[i], ui_values[i])
+
+    def get_value_from_ui(self, prop: Property, ui: PropertyUIOpenCV):
         if prop.type == PropNodeType.hsv:
-            children_ui = ui["children_ui"]
-            self.set_bar_pos(children_ui[0]["name"], children_ui[0]["window_name"], int(prop.value()[0]), True)
-            self.set_bar_pos(children_ui[1]["name"], children_ui[1]["window_name"], int(prop.value()[1] * 255.0), True)
-            self.set_bar_pos(children_ui[2]["name"], children_ui[2]["window_name"], int(prop.value()[2] * 255.0), True)
+            ui_values = self.ui_values_from_ui_children(ui, prop)
+            return float(ui_values[0]), float(ui_values[1]) / 255.0, float(ui_values[2]) / 255.0
         elif prop.type == PropNodeType.rgb:
-            children_ui = ui["children_ui"]
-            for i in range(0, 2):
-                self.set_bar_pos(children_ui[i]["name"], children_ui[i]["window_name"], int(prop.value()[i]), True)
+            ui_values = self.ui_values_from_ui_children(ui, prop)
+            return int(ui_values[0]), int(ui_values[1]), int(ui_values[2])
         else:
-            self.set_bar_pos(ui["name"], ui["window_name"], prop.value(), True)
+            return self.get_bar_pos_for_ui(ui)
+
+    def update_value_for_ui(self, prop: Property, ui: PropertyUIOpenCV):
+        """
+        This function updates the UI for a given property. It will NOT set the property and is
+        garantueed not to have side effects.
+        :param prop:
+        :param ui:
+        :return:
+        """
+        current_state = self.state
+        self.state = "SILENT"  # prevent HIGHGUI from sending us an update that the ui has changed..
+        if prop.type == PropNodeType.hsv:
+            prop_value = prop.value()
+            ui_values = [int(prop_value[0]), int(prop_value[1] * 255.0), int(prop_value[2] * 255.0)]
+            self.set_ui_values_for_ui_children(ui, ui_values)
+        elif prop.type == PropNodeType.rgb:
+            self.set_ui_values_for_ui_children(ui, [int(i) for i in prop.value()])
+        else:
+            self.set_bar_pos_for_ui(ui, prop.value())
+        self.state = current_state
 
     def update_ui(self, node_path: str):
         # print("update_ui : "+node_path)
